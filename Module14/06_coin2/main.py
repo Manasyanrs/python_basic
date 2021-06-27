2e2653c051e6a6c4f38f09351ed831385d568e68
Радик Манасян <arnara@inbox.ru>
2021-06-27 23:06:52 +0300

    Монетка 2

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+print("Введите координаты монетки:")
+x_coordinate = float(input("X "))
+y_coordinate = float(input("Y "))
+radius = float(input("Введите радиус: "))
+
+def metal_detector(coordinate_x, coordinate_y, radius):
+    print()
+    total_sum_x_y = coordinate_x + coordinate_y
+    if radius >= total_sum_x_y >= 0:
+        print("Монетка где-то рядом")
+    else:
+        print("Монетки в области нет")
+
+metal_detector(x_coordinate, y_coordinate, radius)
