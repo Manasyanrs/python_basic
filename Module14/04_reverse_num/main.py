1a5f1f637e185bed228d5c2945546c192b416f3a
Радик Манасян <arnara@inbox.ru>
2021-06-27 19:40:02 +0300

    Число наоборот

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,27 @@
-# TODO здесь писать код
+first_number = float(input("Введите первое число: "))
+second_number = float(input("Введите второе число: "))
+def revers_number(number):
+    first_number = ""
+    second_number = ""
+    flags = True
+    for element in str(number):
+        if element == ".":
+            flags = False
+        elif flags:
+            first_number = element + first_number
+        else:
+            second_number = element + second_number
+
+    total = float(first_number + "." + second_number)
+
+    return total
+
+first_results = revers_number(first_number)
+second_results = revers_number(second_number)
+total_sum_numbers = first_results + second_results
+
+print("Первое число наоборот:", first_results)
+print("Второе число наоборот:", second_results)
+print("Сумма:", total_sum_numbers)
 
 
