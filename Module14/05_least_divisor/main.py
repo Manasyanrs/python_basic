8e5cbb66086e2695a7c7ca0be7aeaf5c075c9d47
Радик Манасян <arnara@inbox.ru>
2021-06-27 22:37:07 +0300

    Наименьший делитель

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+questions = int(input("Введите число: "))
+
+
+def minimum_divisor(number):
+    results = 0
+    for element in range(2, number + 1):
+        if number % element == 0:
+            results = element
+            if results >= 2:
+                break
+    return results
+
+
+total = minimum_divisor(questions)
+
+print("Наименьший делитель, отличный от единицы:", total)
