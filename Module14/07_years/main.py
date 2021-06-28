bda6235af31610d94d6b4a3c1a4f1d0b5ce6c002
Радик Манасян <arnara@inbox.ru>
2021-06-28 14:16:45 +0300

    Года

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+first_year = 1900#int(input("Введите первый год: "))
+second_year = 2100#int(input("Введите второй год: "))
+numbers_2_years = ""
+sum_2_year = str(first_year) + str(second_year)
+
+for element in sum_2_year:
+    if element not in numbers_2_years:
+        numbers_2_years += element
+
+print("Года от", first_year, "до", second_year, "с тремя одинаковыми цифрами:")
+
+count_element = 0
+for element in numbers_2_years:
+    for number in range(first_year, second_year + 1):
+        for number_element in str(number):
+            if element == number_element:
+                count_element += 1
+            else:
+                count_element = count_element
+
+        if count_element == 3:
+            if first_year <= number <= second_year:
+                print(number)
+                count_element = 0
+        else:
+            count_element = 0
