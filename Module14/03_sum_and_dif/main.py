06efee85982613ccb03b9ced26102b89255589c9
Радик Манасян <arnara@inbox.ru>
2021-06-29 19:40:26 +0300

    Сумма и разность

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,1 +1,25 @@
-# TODO не сделали
+def sum_digits_in_number(number):
+    results = 0
+    for element in str(number):
+        results += int(element)
+    return results
+
+
+def count_digits_in_number(number):
+    total = 0
+    for element in str(number):
+        total += 1
+    return total
+
+
+questions = int(input("Введите число: "))
+
+sum_digits = sum_digits_in_number(questions)
+count_digits = count_digits_in_number(questions)
+total_digits = count_digits_in_number(sum_digits) + count_digits_in_number(count_digits)
+difference = sum_digits - count_digits
+
+print("\nСумма цифр:", sum_digits)
+print("Кол-во цифр в числе:", count_digits)
+if total_digits == difference:
+    print("Разность суммы и кол-ва цифр:", difference)
