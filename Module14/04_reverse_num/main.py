2c71f2e93ba3ba99b17750754f0264a68ac86a5f
Радик Манасян <arnara@inbox.ru>
2021-06-29 19:46:09 +0300

    Число наоборот

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,34 +1,27 @@
-first_number = float(input("Введите первое число: "))
-second_number = float(input("Введите второе число: "))
 
-# TODO Параметры как и имя функции не должны подчеркиваться и пересекаться с глобальными переменными
-# TODO функции объявляем всегда выше
 def revers_number(number):
-    first_number = ""
-    second_number = ""
+    first = ""
+    second = ""
     flags = True
     for element in str(number):
         if element == ".":
             flags = False
         elif flags:
-            first_number = element + first_number
+            first = element + first
         else:
-            second_number = element + second_number
+            second = element + second
 
-    total = float(first_number + "." + second_number)
-
+    total = float(first + "." + second)
     return total
+
+
+first_number = float(input("Введите первое число: "))
+second_number = float(input("Введите второе число: "))
 
 first_results = revers_number(first_number)
 second_results = revers_number(second_number)
 total_sum_numbers = first_results + second_results
 
-print("Первое число наоборот:", first_results)
+print("\nПервое число наоборот:", first_results)
 print("Второе число наоборот:", second_results)
 print("Сумма:", total_sum_numbers)
-
-# Начиная с этого модуля буду обращать внимание на то что подчеркивает Пайчар. Придерживаемся PEP8
-# Можно привести все к нужному формату code\Reformat code
-
-# TODO Есть недочеты в форматировании по PEP8, используйте пункт меню в пайчарме
-
