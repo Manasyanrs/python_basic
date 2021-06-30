5ef2ea157f1e4895a54422ca8c1cdd538122ec24
Радик Манасян <arnara@inbox.ru>
2021-06-30 21:44:19 +0300

    удаляет из списка наибольшие элементы

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,16 @@
-# TODO здесь писать код
+questions = int(input("Кол-во видеокарт: "))
+
+old_vga = []
+new_vga = []
+
+for number in range(1, questions + 1):
+    model_vga = int(input(str(number) + " Видеокарта: "))
+    old_vga.append(model_vga)
 
 
+for vga in old_vga:
+    if vga <= 3070:
+        new_vga.append(vga)
+
+print("\nСтарый список видеокарт:", old_vga)
+print("Новый список видеокарт:", new_vga)
