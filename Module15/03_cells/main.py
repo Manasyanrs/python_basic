ef930e33f4dc80fe0ed6747d152ccf5546f012d8
Радик Манасян <arnara@inbox.ru>
2021-06-30 21:32:55 +0300

    значения элементов которых меньше их индекса

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+
+questions = int(input("Кол-во клеток: "))
+improper_cell = []
+print()
+for numbers in range(1, questions + 1):
+    new_questions = int(input("Эффективность " + str(numbers) + " клетки: "))
+    if numbers > new_questions:
+        improper_cell.append(new_questions)
+
+print("\nНеподходящие значения:", end = " ")
+for improper in improper_cell:
+    print(improper, end = " ")
