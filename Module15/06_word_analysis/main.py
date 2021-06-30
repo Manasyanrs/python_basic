60bb2816416a084428bac5f91a3fa4df487901cd
Радик Манасян <arnara@inbox.ru>
2021-06-30 23:04:25 +0300

    Количество уникальных букв

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+def count_unique_letters(word):
+    count_letters = 0
+    for letters in word:
+        total = 0
+        for element in word:
+            if letters == element:
+                total += 1
+        if total == 1:
+            count_letters += total
+            total = 0
+
+    print("\nКол-во уникальных букв:", count_letters)
+
+
+questions = input("Введите слово: ")
+
+count_unique_letters(questions)
