af1fd08ed41d76d432deb30692fc9c7c73e846a7
Радик Манасян <arnara@inbox.ru>
2021-06-30 21:00:04 +0300

    выводим элементы списка с чётными индексами

--- Module15/02_tournament/main.py
+++ Module15/02_tournament/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+
+names = ["Артемий", "Борис", "Влад", "Гоша", "Дима", "Евгений", "Женя", "Захар"]
+
+even_names = []
+
+for name_id in range(len(names)):
+
+    if name_id % 2 == 1:
+        even_names.append(names[name_id])
+
+print("Список имен\n", names)
+print("\nКаждое второе имя из списка\n", even_names)
