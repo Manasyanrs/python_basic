3f8b2c012ab775afc3f1b4a4dabdbc0eadb417b9
Радик Манасян <arnara@inbox.ru>
2021-06-30 20:43:40 +0300

    формируем список из нечетных чисел от 1 до N.

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+
+def odd_numbers(number):
+    odd_number = []
+
+    for numbers in range(1, number + 1):
+        if numbers % 2 == 1:
+            odd_number.append(numbers)
+    return odd_number
+
+
+questions = int(input('Введите число: '))
+
+print("Список из нечетных чисел")
+print(odd_numbers(questions))
+
