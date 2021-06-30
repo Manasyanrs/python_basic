c1a7c20bf9a99b6dc622e6553bcb380eaca5267a
Радик Манасян <arnara@inbox.ru>
2021-07-01 00:44:47 +0300

    Сдвиг элементов в списке

--- Module15/08_running_nums/main.py
+++ Module15/08_running_nums/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+
+my_element = [1, 4, -3, 0, 10]
+
+shift = int(input("Сдвиг: "))
+
+print("\nИзначальный список:", my_element)
+new_element = []
+for _ in range(len(my_element)):
+    if shift == 1:
+        new_element = my_element[shift:] + my_element[:shift]
+    else:
+        new_element = my_element[shift - 1:] + my_element[:shift - 1]
+print("Сдвинутый список:", new_element)
