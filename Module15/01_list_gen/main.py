532a7a7d6b29b6a8f1f2ca42457e856a783f80e3
Радик Манасян <arnara@inbox.ru>
2021-07-01 22:23:01 +0300

    список нечетных чисел

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -11,8 +11,8 @@ def odd_numbers(number):
 questions = int(input('Введите число: '))
 
 print("Список из нечетных чисел")
-# TODO старайтесь не вызывать функцию в принте
-print(odd_numbers(questions))
+odd_numbers = odd_numbers(questions)
+print(odd_numbers)
 
 
 
