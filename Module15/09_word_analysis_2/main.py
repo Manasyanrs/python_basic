386465ee882a88b123f2ad2389a91aca74307e07
Радик Манасян <arnara@inbox.ru>
2021-07-01 12:42:07 +0300

    Слово палиндромом или нет

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+word = input("Введите слово: ")
+revers_word = word[::-1]
+count_letters = 0
+
+for letters in range(len(word)):
+    if word[letters] == revers_word[letters]:
+        count_letters += 1
+    else:
+        count_letters -= 1
+
+if count_letters == len(word):
+    print("Слово является палиндромом")
+
+else:
+    print("Слово не является палиндромом")
+
