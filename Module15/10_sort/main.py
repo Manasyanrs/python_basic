505fdeffab560aed603d205de314844e2b129e86
Радик Манасян <arnara@inbox.ru>
2021-07-01 15:07:10 +0300

    Отсортированный список

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+initial = [1, 4, -3, 0, 10]
+print("Изначальный список:", initial)
+
+for index in range(len(initial)):
+    for element in range(index + 1, len(initial)):
+        if initial[index] > initial[element]:
+            initial[index], initial[element] = initial[element], initial[index]
+
+print("Отсортированный список:", initial)
+# part 2
+print()
+initial.sort()
+print("Отсортированный список:", initial)
