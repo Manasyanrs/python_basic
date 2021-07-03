ec59615079ec29713255682ed1f476d6531b0cac
Радик Манасян <arnara@inbox.ru>
2021-07-03 18:50:07 +0300

    подсчет одного вида деталей и стоимость

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,4 +2,13 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
-# TODO здесь писать код
+questions = input("Название детали: ")
+names_products = 0
+price_products = 0
+for products in shop:
+    if products[0] == questions:
+        names_products += 1
+        price_products += products[1]
+
+print("Кол-во деталей -", names_products)
+print("Общая стоимость -", price_products)
