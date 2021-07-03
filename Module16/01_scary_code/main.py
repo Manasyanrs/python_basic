19bf2aab31e3d3ccc41ff048884d8b88ead85fe0
Радик Манасян <arnara@inbox.ru>
2021-07-03 17:37:39 +0300

    объединении списков и подсчет n-ого числа в списке

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,13 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+first_number = [1, 5, 3]
+second_number = [1, 5, 1, 5]
+third_number = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+first_number.extend(second_number)
+count_numbers = first_number.count(5)
+print("Кол-во цифр 5 при первом объединении:", count_numbers)
+
+first_number.extend(third_number)
+count_numbers = first_number.count(3)
+print("Кол-во цифр 3 при втором объединении:", count_numbers)
+
+print("Итоговый список:", first_number)
