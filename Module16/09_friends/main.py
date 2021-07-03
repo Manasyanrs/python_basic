859fbcc3f9a863f94237d7eb4198b70a002338d4
Радик Манасян <arnara@inbox.ru>
2021-07-03 20:17:47 +0300

    долговое обязательство между друзьями

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+count_friends = int(input("Кол-во друзей: "))
+count_friends = [0] * count_friends
+debt = int(input("Долговых расписок: "))
+
+print()
+for number in range(1, debt + 1):
+    print(number, "расписка")
+    whom = int(input("Кому: "))
+    from_whom = int(input("От кого: "))
+    price = int(input("Сколько: "))
+
+    if whom:
+        count_friends[whom - 1] -= price
+        count_friends[from_whom - 1] += price
+    print()
+
+print("\nБаланс друзей:")
+for money in range(len(count_friends)):
+    number_friends = money + 1
+    print(number_friends, ":", count_friends[money])
