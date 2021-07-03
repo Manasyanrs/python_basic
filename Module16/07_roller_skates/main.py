97a0d3290b99da737c79309c89a63df93f0f94d8
Радик Манасян <arnara@inbox.ru>
2021-07-03 19:31:19 +0300

    подсчет людей с роликами

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+count_rollers = int(input("Кол-во коньков: "))
+rollers = []
+
+for number in range(1, count_rollers + 1):
+    size = int(input("Размер " + str(number) + " пары: "))
+    rollers.append(size)
+
+print()
+peoples = int(input("Кол-во людей: "))
+count_people = 0
+
+for count in range(1, peoples + 1):
+    size_foot = int(input("Размер ноги " + str(count) + " человека: "))
+    if size_foot in rollers:
+        rollers.remove(size_foot)
+        count_people += 1
+
+print("\nНаибольшее кол-во людей, которые могут взять ролики:", count_people)
