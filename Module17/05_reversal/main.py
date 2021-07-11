816add9a46b3dcfc35ab7e97c6e413a07b7b3327
Радик Манасян <arnara@inbox.ru>
2021-07-11 17:56:48 +0400

    разворот элементов между буквами "h"

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,19 @@
-# TODO здесь писать код
+text = input("Введите текст: ")
+text = text[::-1]
+
+count_h = []
+for letters in range(len(text)):
+    if text[letters] == "h" or text[letters] == "H":
+        count_h.append(letters)
+
+print("\nТекст в зеркальном виде", text)
+
+if len(count_h) < 2:
+    print("Количество буквы 'h' меньше двух")
+
+elif len(count_h) == 2:
+    print("Элементы между буквами 'h'", text[count_h[0] + 1:count_h[1]])
+
+else:
+    for index in range(len(count_h) - 1):
+        print("Элементы между буквами 'h'", text[count_h[index] + 1:count_h[index + 1]])
