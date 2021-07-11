68b4c98e24836e7eb941e15650f6de51c3d9db6b
Радик Манасян <arnara@inbox.ru>
2021-07-11 17:39:41 +0400

    количество гласных букв в тексте

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+text = input("Введите текст: ")
+
+vowels = [letters for letters in text if letters == "а" or
+          letters == "у" or letters == "о" or letters == "ы" or
+          letters == "и" or letters == "э" or
+          letters == "я" or letters == "ю" or letters == "е"]
+
+print("Список гласных букв:", vowels)
+print("Длина списка:", len(vowels))
