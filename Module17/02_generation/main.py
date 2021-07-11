ece8d537628707b42f8693e36a3c1f0ceceac445
Радик Манасян <arnara@inbox.ru>
2021-07-11 17:49:53 +0400

    Генерация

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,1 +1,4 @@
-# TODO здесь писать код
+number = int(input("Введите длину списка: "))
+
+results = [(digit * 0 + 1 if digit % 2 == 0 else digit % 5) for digit in range(number)]
+print("Результат:", results)
