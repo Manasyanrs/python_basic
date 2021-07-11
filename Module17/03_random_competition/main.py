6fc6dd561106a5cf8b8c84cab25551af94f5a2df
Радик Манасян <arnara@inbox.ru>
2021-07-11 17:52:30 +0400

    сравнение элементов двух списков

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import random
+
+first_command = [random.uniform(5, 10) for _ in range(20)]
+first = [round(numbers, 2) for numbers in first_command]
+
+second_command = [random.uniform(5, 10) for _ in range(20)]
+second = [round(numbers, 2) for numbers in second_command]
+
+results = [(first[digit] if first[digit] > second[digit] else second[digit]) for digit in range(20)]
+
+print("Первая команда:", first)
+print("Вторая команда:", second)
+print("Победители тура:", results)
