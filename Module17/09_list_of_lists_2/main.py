b5ea9626d07992878704e7355594c98f8b5e1825
Радик Манасян <arnara@inbox.ru>
2021-07-12 20:16:02 +0400

    доработанное домашнее задание

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,10 +1,7 @@
+nice_numbers = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+                [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO list set dict tuple в именовании не используем
-
-nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-             [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
-
-results = [third_level for first_level in nice_list
+results = [third_level for first_level in nice_numbers
            for second_level in first_level
            for third_level in second_level]
 print(results)
