2599261ca525c38b7cc9049ae7be55f3e9e4da38
Радик Манасян <arnara@inbox.ru>
2021-07-14 13:04:39 +0400

    доработанное домашнее задание

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -2,7 +2,6 @@ text = input("Введите текст: ")
 
 vowels_letters = "ауоыиэяюе"
 results = [letters for letters in text for vowels in vowels_letters if vowels == letters]
-
+count_symbols = len(results)
 print("Список гласных букв:", results)
-# TODO len тоже не используем в принте по возможности
-print("Длина списка:", len(results))
+print("Длина списка:", count_symbols)
--- Module17/08_entertainment/main.py
+++ Module17/08_entertainment/main.py
@@ -4,9 +4,8 @@ count_sticks = int(input("Количество палок: "))
 count_throws = int(input("Количество бросков: "))
 stick_number = list(range(1, count_sticks + 1))
 
-# TODO за что у вас отвечает i
-for i in range(1, count_throws + 1):
-    print("Бросок " + str(i) + ".", end=" ")
+for throws in range(1, count_throws + 1):
+    print("Бросок " + str(throws) + ".", end=" ")
     left = random.randint(2, count_sticks)
     right = random.randint(left, count_sticks)
     print("Сбиты палки с номера", left, "\nпо номер", right)
