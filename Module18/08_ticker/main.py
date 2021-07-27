f13c56e0ad3e50975ac587cfb03eaf0d56ddd9dd
Радик Манасян <arnara@inbox.ru>
2021-07-27 22:06:26 +0300

    Бегущая строка

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+first_line = input("Первая строка: ")
+second_line = input("Вторая строка: ")
+count = 0
+results = ""
+if first_line == second_line:
+    print("Первая строка идентичен к второму строку")
+
+else:
+    for i in range(1, len(second_line) + 1):
+        count += 1
+        results = results + second_line[-i:] + second_line[:-i]
+        if results == first_line:
+            print("Первая строка получается из второй со сдвигом", count)
+            break
+
+        else:
+            results = ""
+
+if len(second_line) == count:
+    print("Первую строку нельзя получить из второй с помощью циклического сдвига.")
