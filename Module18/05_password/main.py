7c834ef0c30596ab5dba3515bb222c7a4d34d6e1
Радик Манасян <arnara@inbox.ru>
2021-07-27 22:02:18 +0300

    надёжный пароль

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+while True:
+    password = input("Придумайте пароль: ")
+    digit = 0
+    upper_letters = 0
+
+    for element in password:
+        if element.isdigit():
+            digit += 1
+        elif element.isupper():
+            upper_letters += 1
+
+    if len(password) < 8:
+        print("Пароль должен состоять минимум из 8 символов")
+    elif digit < 3:
+        print("Пароль ненадёжный. Попробуйте ещё раз.")
+    elif upper_letters < 1:
+        print("Пароль ненадёжный. Попробуйте ещё раз.")
+    else:
+        print("Это надёжный пароль!")
+        break
