cb06e3628de97f4cf496a7006342e2084f5959af
Радик Манасян <arnara@inbox.ru>
2021-07-27 21:57:30 +0300

    Самое длинное слово и его длина

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+text = input("Введите строку: ").split()
+
+word = ""
+length_word = 0
+for element in text:
+    if len(element) > length_word:
+        length_word = len(element)
+        word = element
+
+print("\nСамое длинное слово в тексте", word, "\nколичество букв в слове", word, "=", length_word)
