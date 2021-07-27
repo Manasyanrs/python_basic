c33ee1f5f66a5e88fdc79427d8ac535696d9e338
Радик Манасян <arnara@inbox.ru>
2021-07-27 22:04:50 +0300

    правильный IP-адрес

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+print("Числа Ip разделить точкой")
+ipaddress = input("Введите IP: ")
+new_ip = ""
+for element in ipaddress:
+    if element != ".":
+        new_ip += element
+    else:
+        new_ip += " "
+
+new_ip = new_ip.split()
+count_true_iterations = 0
+for digit in new_ip:
+    if digit:
+        if "," in digit:
+            print("Адрес - это четыре числа, разделенные точками")
+            break
+        elif not digit.isdigit():
+            print(digit + "- не целое число")
+            break
+        elif int(digit) > 255:
+            print(digit, " превышает 255")
+            break
+        count_true_iterations += 1
+
+if count_true_iterations == 4:
+    print("IP-адрес корректен")
