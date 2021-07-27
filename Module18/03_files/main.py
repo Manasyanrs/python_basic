231590c347dd3c42f40ed8267f584ffa542b4ba6
Радик Манасян <arnara@inbox.ru>
2021-07-27 21:59:15 +0300

    файла не должно начинаться символом

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+name_file = input("Название файла: ")
+
+if not name_file.endswith(".txt") and not name_file.endswith(".docx"):
+    print("Ошибка: неверное расширение файла. Ожидалось .txt или .docx")
+elif name_file[0] == "@" or name_file[0] == "№" or \
+        name_file[0] == "$" or name_file[0] == "%" or \
+        name_file[0] == "^" or name_file[0] == "&" or \
+        name_file[0] == "*" or name_file[0] == "(" or name_file[0] == ")":
+    print("Ошибка: название начинается на один из специальных символов")
+else:
+    print("Файл назван верно.")
