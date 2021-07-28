dc00af0ad6b0a0abb1a020e5be2c37e272bbd787
Радик Манасян <arnara@inbox.ru>
2021-07-28 22:38:25 +0300

    доработанный дз

--- Module18/08_ticker/main.py
+++ Module18/08_ticker/main.py
@@ -6,9 +6,9 @@ if first_line == second_line:
     print("Первая строка идентичен к второму строку")
 
 else:
-    for i in range(1, len(second_line) + 1):
+    for index in range(1, len(second_line) + 1):
         count += 1
-        results = results + second_line[-i:] + second_line[:-i]
+        results = results + second_line[-index:] + second_line[:-index]
         if results == first_line:
             print("Первая строка получается из второй со сдвигом", count)
             break
--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,31 +1,28 @@
-
-# TODO чтобы не использовать символ \
-# TODO заключите весь текст в """ """
-text = "vujgvmCfb tj ufscfu ouib z/vhm " \
-       "jdjuFyqm jt fscfuu uibo jdju/jnqm " \
-       "fTjnqm tj scfuuf ibou fy/dpnqm " \
-       "yDpnqmf jt cfuufs boui dbufe/dpnqmj " \
-       "uGmb tj fuufsc ouib oftufe/ " \
-       "bstfTq jt uufscf uibo otf/ef " \
-       "uzSfbebcjmj vout/dp " \
-       "djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf " \
-       "ipvhiBmu zqsbdujdbmju fbutc uz/qvsj " \
-       "Fsspst tipvme wfsof qbtt foumz/tjm " \
-       "omfttV mjdjumzfyq odfe/tjmf " \
-       "Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv " \
-       "Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ " \
-       "Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( " \
-       "i/Evud " \
-       "xOp tj scfuuf ibou /ofwfs " \
-       "uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op " \
-       "gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb " \
-       "Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ " \
-       "bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf'uip".lower().split()
+text = """vujgvmCfb tj ufscfu ouib z/vhm 
+       jdjuFyqm jt fscfuu uibo jdju/jnqm
+       fTjnqm tj scfuuf ibou fy/dpnqm
+       yDpnqmf jt cfuufs boui dbufe/dpnqmj
+       uGmb tj fuufsc ouib oftufe/
+       bstfTq jt uufscf uibo otf/ef
+       uzSfbebcjmj vout/dp
+       djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf
+       ipvhiBmu zqsbdujdbmju fbutc uz/qvsj
+       Fsspst tipvme wfsof qbtt foumz/tjm
+       omfttV mjdjumzfyq odfe/tjmf
+       Jo fui dfgb pg hvjuz-bncj gvtfsf fui ubujpoufnq up ftt/hv
+       Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/
+       Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud
+       xOp tj scfuuf ibou /ofwfs
+       uipvhiBm fsofw jt fopgu cfuufs boui iu++sjh x/op
+       gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb
+       Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/
+       bnftqbdftO bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf'uip""".split()
 
 lower_alphabet = "abcdefghijklmnopqrstuvwxyz"
-symbols = "!@#$%^&*()_-+=,.:/'"
+upper_alphabet = lower_alphabet.upper()
+
+symbols = """!'(*+,-./"""
 results = ""
-sentence = ""
 my_step = -3
 for words in text:
     step = my_step
@@ -35,23 +32,24 @@ for words in text:
         letter = words[step]
         step += 1
         count_letters_in_word += 1
-        if letter in symbols:
-            sentence += letter
 
+        if letter in symbols:
+            index = symbols.index(letter)
+            results += symbols[index - 1]
+        elif letter.isupper():
+            index = upper_alphabet.index(letter)
+            results += upper_alphabet[index - 1]
         else:
             index = lower_alphabet.index(letter)
-            sentence += lower_alphabet[index - 1]
+            results += lower_alphabet[index - 1]
 
         if count_letters_in_word == len(words):
-            sentence += " "
+            results += " "
             if "/" in words:
+                results += "\n"
                 my_step -= 1
-                results += "\n" + sentence.capitalize()
-                sentence = ""
 
 print(results)
-
-# TODO результат должен быть таким, есть нераскодированные символы
 
 # Beautiful is better than ugly.
 # Explicit is better than implicit.
@@ -72,4 +70,3 @@ print(results)
 # If the implementation is hard to explain, it's a bad idea.
 # If the implementation is easy to explain, it may be a good idea.
 # Namespaces are one honking great idea -- let's do more of those!
-
