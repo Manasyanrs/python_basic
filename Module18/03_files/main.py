dff214b7e2f0502ac2a5887c32feb5018fc65a28
user74 <housechel@mail.ru>
2021-07-28 19:57:43 +0500

    Lesson 018 03,10 - Corrected

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -2,6 +2,8 @@ name_file = input("Название файла: ")
 
 if not name_file.endswith(".txt") and not name_file.endswith(".docx"):
     print("Ошибка: неверное расширение файла. Ожидалось .txt или .docx")
+# TODO знак для переноса \ не используем в коде
+# TODO все символы вынести в строку и проверять на вхождение в нее
 elif name_file[0] == "@" or name_file[0] == "№" or \
         name_file[0] == "$" or name_file[0] == "%" or \
         name_file[0] == "^" or name_file[0] == "&" or \
--- Module18/10_the_truth/main.py
+++ Module18/10_the_truth/main.py
@@ -1,3 +1,6 @@
+
+# TODO чтобы не использовать символ \
+# TODO заключите весь текст в """ """
 text = "vujgvmCfb tj ufscfu ouib z/vhm " \
        "jdjuFyqm jt fscfuu uibo jdju/jnqm " \
        "fTjnqm tj scfuuf ibou fy/dpnqm " \
@@ -47,3 +50,26 @@ for words in text:
                 sentence = ""
 
 print(results)
+
+# TODO результат должен быть таким, есть нераскодированные символы
+
+# Beautiful is better than ugly.
+# Explicit is better than implicit.
+# Simple is better than complex.
+# Complex is better than complicated.
+# Flat is better than nested.
+# Sparse is better than dense.
+# Readability counts.
+# Special cases aren't special enough to break the rules.
+# Although practicality beats purity.
+# Errors should never pass silently.
+# Unless explicitly silenced.
+# In the face of ambiguity, refuse the temptation to guess.
+# There should be one-- and preferably only one --obvious way to do it.
+# Although that way may not be obvious at first unless you're Dutch.
+# Now is better than never.
+# Although never is often better than *right* now.
+# If the implementation is hard to explain, it's a bad idea.
+# If the implementation is easy to explain, it may be a good idea.
+# Namespaces are one honking great idea -- let's do more of those!
+
