576f2e3b8a9bf601eb82266d1ebabfd2528f24e1
Радик Манасян <arnara@inbox.ru>
2021-08-06 19:30:00 +0300

    функция сортирует кортеж по возрастанию, если один элемент не является целим числом возврашает исходной кортеж

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -1,1 +1,20 @@
-# TODO здесь писать код
+def float_in_tuple(tuple_file):
+    new_tuple = sorted(tuple_file)
+    count_float = 0
+    for digit in new_tuple:
+        if type(digit) == float:
+            count_float += 1
+            break
+
+    if count_float == 0:
+        print("В кортеже нету числа с точкой")
+        return new_tuple
+    else:
+        print("В кортеже есть число с точкой")
+        return tuple_file
+
+
+total_numbers = (1, 8, 5, 4, 9, 25, 6.5)
+
+result = float_in_tuple(total_numbers)
+print(result)
