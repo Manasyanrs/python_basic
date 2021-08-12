6592e4687dccababfc3166fcedece3abfbdded79
Радик Манасян <arnara@inbox.ru>
2021-08-12 13:36:27 +0300

    функция выводит числа от 1 до n

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+def digits_1_to_n(number):
+    if number == 2:
+        return 1
+    elif number == 1 or number == 0:
+        return 0
+    else:
+        if number > 0:
+            print(digits_1_to_n(number - 1), end=" ")
+            return number - 1
+        else:
+            if number == -2:
+                return -1
+            elif number == -1:
+                return 0
+            else:
+                print(digits_1_to_n(number + 1), end=" ")
+                return number + 1
+
+
+question = int(input("Введите число: "))
+result = digits_1_to_n(question)
+print(result)
