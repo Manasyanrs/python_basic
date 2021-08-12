434c82f6e54480f9038d355d96cf5f51d37f88ea
Радик Манасян <arnara@inbox.ru>
2021-08-12 13:43:38 +0300

    Ускоряем работу функции

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,9 +1,22 @@
 def calculating_math_func(data):
     result = 1
-    for index in range(1, data + 1):
-        result *= index
+    if data in factorials.keys():
+        result = factorials[data]
+    else:
+        for index in range(1, data + 1):
+            result *= index
+        factorials[data] = result
+
     result /= data ** 3
     result = result ** 10
     return result
 
-# TODO оптимизировать функцию
+
+factorials = dict()
+
+test_1 = calculating_math_func(5)
+print(test_1)
+test_2 = calculating_math_func(10)
+print(test_2)
+test_3 = calculating_math_func(5)
+print(test_3)
