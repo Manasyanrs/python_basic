c4084ddb4858f10566de4467cec0ff28ca2d85b4
Радик Манасян <arnara@inbox.ru>
2021-08-17 22:45:55 +0300

    Дзен Пайтона

--- Module22/02_zen_of_python/main.py
+++ Module22/02_zen_of_python/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+text = open("zen.txt", "r")
+
+text_line = text.readlines()
+text_line[-1] += "\n"
+
+for line in range(1, len(text_line) + 1):
+    print(text_line[-line], end="")
+
+text.close()
