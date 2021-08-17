fa4ad87503dbb872f3580a2f1d79a206acf6d2b1
Радик Манасян <arnara@inbox.ru>
2021-08-17 22:55:01 +0300

    Частотный анализ

--- Module22/08_frequency_analysis/main.py
+++ Module22/08_frequency_analysis/main.py
@@ -1,1 +1,37 @@
-# TODO здесь писать код
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+file = open("text.txt", "r")
+print("Содержимое файла", file.name)
+text = file.read().lower()
+print(text)
+count_elements = 0
+text_letters = ""
+maximum_letters = ""
+for element in text:
+    if element in alphabet:
+        count_elements += 1
+        if text.count(element) > 1:
+            if element not in maximum_letters:
+                maximum_letters += element
+        else:
+            text_letters += element
+
+text_letters = sorted(text_letters)
+maximum_letters = sorted(maximum_letters)
+total_letters = maximum_letters + text_letters
+
+result = ""
+for letter in total_letters:
+    analysis_frequency = text.count(letter) / count_elements
+    if letter not in result:
+        result += letter + " {:0.3f} \n".format(analysis_frequency)
+
+file.close()
+
+second_file = open("analysis.txt", "w")
+second_file.write(result)
+second_file.close()
+
+total_result = open("analysis.txt", "r")
+print("\nСодержимое файла", total_result.name)
+print(total_result.read())
+total_result.close()
