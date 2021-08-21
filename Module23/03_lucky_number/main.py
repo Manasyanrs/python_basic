e6d38c8f4a6d18a63978a794caad0bf070464042
Радик Манасян <arnara@inbox.ru>
2021-08-21 15:38:55 +0300

    запрашивается число до тех пор, пока сумма этих чисел не станет больше либо равна 777

--- Module23/03_lucky_number/main.py
+++ Module23/03_lucky_number/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+total_result = 0
+count = 0
+while True:
+    if total_result >= 777:
+        print("Сумма чисел больше либо равна 777.")
+        break
+    try:
+        if count == 13:
+            raise Warning("Программа приостановлено случанием исключением")
+        question = int(input("Введите число: "))
+        total_result += question
+        with open("numbers.txt", "a") as digit:
+            digit.write("{}\n".format(str(question)))
+    except ValueError:
+        print("Введите целое число.")
+    except Warning:
+        raise Warning("Программа выбрасывала с вероятностью 1 к 13")
