09913f3ca4d499c951ad038553a7b568b45da32e
Радик Манасян <arnara@inbox.ru>
2021-08-21 15:43:31 +0300

    Текстовый калькулятор

--- Module23/05_text_calc/main.py
+++ Module23/05_text_calc/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+def calculate(info):
+    try:
+        first = int(info[0])
+        second = int(info[2])
+        if info[1] == "+":
+            print(first + second)
+        if info[1] == "/":
+            print(first / second)
+        if info[1] == "%":
+            print(first % second)
+        if info[1] == "//":
+            print(first // second)
+    except ValueError:
+        print("Операнды должны быть числомы.")
+    except ZeroDivisionError:
+        print("На 0 делить нельзя.")
+
+
+try:
+    with open("masige.txt", "r") as calc:
+        for information in calc.readlines():
+            calculate(information.split())
+except FileNotFoundError:
+    print("Файл не найден")
