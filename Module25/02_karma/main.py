5b57bfbfa040f9498f7da3510a6c18de1b1bac36
user74 <housechel@mail.ru>
2021-09-19 13:10:52 +0500

    Lesson 025 02,03 - Corrected

--- Module25/02_karma/main.py
+++ Module25/02_karma/main.py
@@ -22,31 +22,40 @@ class DepressionError(Exception):
 
 
 def one_day(file):
+    # TODO karma не должна быть подчеркнута
     karma = random.randint(1, 7)
+    # TODO от 1 до 13
     error = random.randint(1, 10)
 
+    # TODO наоборот если error == 1 то мы
     if error != 1:
+        # TODO через exception = random.choice(EXCEPTIONS)
+        # TODO и тут зарейзим exception добавим к нему () потому что это вызов объекта Exception
         return karma
-
+    # TODO без елсе
     else:
         # TODO далее условие если еррор равен 13 то мы choice выбираем случайное исключение из списка
         # TODO и его рейзим как объект используя ()
         result = random.choice(file)
-        # TODO не понимаю как можно к result-у исползовать ()
+        #  не понимаю как можно к result-у исползовать ()
         raise result
 
 
+# TODO сделать константой поместить выше функции
 exceptions = [KillError, DrunkError, CarCrashError, GluttonyError, DepressionError]
+
 karma_points = 0
 errors = []
 while karma_points <= 500:
+    # TODO тут должен быть блок try exception
+    # TODO код ниже переписать отталкиваясь от новой one_day
     point = one_day(exceptions)
 
     if isinstance(point, int):
         karma_points += point
 
     else:
-        # TODO не пойму что вернет point чтобы реолизовать блок else
+        # не пойму что вернет point чтобы реолизовать блок else
         errors.append(str(point))
         if point == KillError:
             raise KillError("KillError")
--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,3 +1,10 @@
+
+# TODO вот такая реализация в инит ничего не добавляем потому что можем сломать логику
+# class MyDict(dict):
+#     def get(self, key, default=0):
+#         return super().get(key, default)
+
+
 class MyDict(dict):
     def __init__(self, file):
         self.file = file
