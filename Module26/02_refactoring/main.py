85010418d8979d7831a067d7ce81312fbc09dede
Радик Манасян <arnara@inbox.ru>
2021-09-21 19:37:28 +0300

    Рефакторинг

--- Module26/02_refactoring/main.py
+++ Module26/02_refactoring/main.py
@@ -1,17 +1,20 @@
+from collections.abc import Iterable
+
+
+def generator(file_1: list, file_2: list) -> Iterable:
+    for digit in file_1:
+        for number in file_2:
+            result = digit * number
+            print("{} * {} = {}".format(digit, number, result))
+            yield result
+
+
 list_1 = [2, 5, 7, 10]
 list_2 = [3, 8, 4, 9]
 to_find = 56
 
-can_continue = True
-for x in list_1:
-    for y in list_2:
-        result = x * y
-        print(x, y, result)
-        if result == to_find:
-            print('Found!!!')
-            can_continue = False
-            break
-    if not can_continue:
+result = generator(file_1=list_1, file_2=list_2)
+for information in result:
+    if information == to_find:
+        print("Found!!!")
         break
-
-# TODO провести рефакторинг кода
