c01df2aff23fbc5638d6cb11455af2b8896cd0fa
Радик Манасян <arnara@inbox.ru>
2021-09-26 19:04:16 +0300

    декоратор считающий количество вызовов декорируемой функции.

--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+from typing import Callable, Any
+
+
+def counter(function: Callable) -> Any:
+    """Декоратор считающий и выводящий количество вызовов декорируемой функции."""
+
+    def wrapper(arg: int) -> str:
+        # TODO Не получается увеличить счетчик
+        count = 0
+        count += 1
+        return "{} {}".format(function(arg), count)
+
+    return wrapper
+
+
+@counter
+def even_number(digit: int) -> str:
+    print(digit, end=" ")
+    return "Количество вызовов декорируемой функции"
+
+
+for digit in range(10):
+    if digit % 2 == 0:
+        print(even_number(digit))
