1b234e3b196329d2db483678a626d3ed6c55b0ba
Радик Манасян <arnara@inbox.ru>
2021-09-28 15:41:31 +0300

    Доработанное дз

--- Module27/03_logging/main.py
+++ Module27/03_logging/main.py
@@ -1,68 +1,48 @@
 import datetime
 from typing import Callable, Any
 import functools
+import logging
 
 
-# TODO дать более явное название
-def decoration(function: Callable) -> Any:
+logging.basicConfig(filename="function_errors.log", level=logging.ERROR)
+
+
+def find_error(function: Callable) -> Any:
     """ Декаратор обработовает пераданную функцию если находит ошыбки
     то в файл function_errors.log записываются название функции и название ошибки
     Используя модуль datetime также записоваем дату и время возникновения ошибки
     :param function: Принимает на обработку итеруемый объект
-    :return: list
+    :return: None
     """
     @functools.wraps(function)
     def wrapper(*args, **kwargs) -> Any:
         data_time = datetime.datetime.now()
-        errors = ""
-        # TODO  логирование можно сразу сделать тут
+        print("Названые функции {function_name}\n"
+              "Документация функции {function_doc}\n".format(
+                function_name=function.__name__,
+                function_doc=function.__doc__))
 
         try:
-            # TODO лучше записать рещультат работы в переменную
-            function(*args, **kwargs)
-
+            result = function(*args, **kwargs)
+            return result
         except TypeError:
-            # TODO и залогировать ошибку тут используя модуль logging
+            # TODO  обработает толко первую ошибку
+            logging.exception("\nНазваные функции {function_name}\n"
+                              "Названые ошибки {errors_name}\n"
+                              "Дата и время ошибки {data_time}\n".format(
+                                function_name=function.__name__,
+                                errors_name=TypeError,
+                                data_time=data_time))
 
-            # TODO знак для переноса не используем в коде
-            errors += "Названые функции {function_name}\n" \
-                     "Названые ошибки {errors_name}\n" \
-                     "Дата и время ошибки {data_time}\n".format(
-                        function_name=function.__name__,
-                        errors_name=TypeError,
-                        data_time=data_time)
+        print("Программа завершилься")
 
-        with open("function_errors.log", "a", encoding="utf-8") as error:
-            error.write(str(errors))
-        # TODO тут мы должны вернуть работу переменной полученной выше
-        return "Программа завершилься"
     return wrapper
 
 
-def logging(function: Callable) -> Any:
-    """Декаратор логирует название функции и её документацию"""
-    @functools.wraps(function)
-    def wrapper(*args, **kwargs) -> Any:
-        print("Названые функции {function_name}\n"
-              "Документация функции {function_doc}\n"
-              "Позиционные аргументы {args}\n"
-              "Именованные аргументы {kwargs}".format(
-                function_name=function.__name__,
-                function_doc=function.__doc__,
-                args=args,
-                kwargs=kwargs))
-    return wrapper
-
-
-#  при логирование не работает декоратор
-#  а без логирование не получается обработать ошибки
-@logging
-@decoration
+@find_error
 def is_digit(file: Any) -> Any:
     for element in file:
-        if element % 2 == 0:
-            print(element)
-        else:
+        if element % 2 != 0:
             raise TypeError
 
 
--- Module27/05_str_count/main.py
+++ Module27/05_str_count/main.py
@@ -5,13 +5,10 @@ def counter(function: Callable) -> Any:
     """Декоратор считающий и выводящий количество вызовов декорируемой функции."""
 
     def wrapper(arg: int) -> str:
-        # TODO так тут он равен 0
-        count = 0
-        # TODO а вот тут 1 и так всегда
-        count += 1
-        return "{} {}".format(function(arg), count)
-    # TODO нужно использовать глобальный параметр функции wrapper и объявить его тут
-    # TODO wrapper.count = 0
+        wrapper.count += 1
+        return "{} {}".format(function(arg), wrapper.count)
+
+    wrapper.count = 0
 
     return wrapper
 
