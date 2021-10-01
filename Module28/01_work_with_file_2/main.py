0b183a9be0c8eafe6be1c2935638e3a3567f7eae
Радик Манасян <arnara@inbox.ru>
2021-10-01 20:03:45 +0300

    Работа с файлом 2

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+class File:
+    """
+    Класс контекст менеджера File,который автоматически создаёт и открывает файл в режиме записи.
+    На выходе из менеджера подавляются все исключения, связанные с файлами.
+
+    Args:
+
+         file_name: str (Передается имя файла)
+         method: str (Передается режиме записи)
+
+    """
+
+    def __init__(self, file_name: str, method: str) -> None:
+        self.__file_name = file_name
+        self.__method = method
+
+    def __enter__(self):
+        self.result = open(self.__file_name, self.__method)
+        return self.result
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        self.result.close()
+        print("Программа шавершена:")
+        return True
+
+
+with File(file_name="example.txt", method="w") as file:
+    file.write("Всем привет!")
