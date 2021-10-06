45b01ea0848800053765f9f7caa5cb4509717b2c
user74 <housechel@mail.ru>
2021-10-06 21:55:07 +0500

    Lesson 028 03 - Corrected

--- Module28/03_modeling/main.py
+++ Module28/03_modeling/main.py
@@ -37,12 +37,19 @@ class Triangle:
         self.height = height
         self.base = base
 
+    # TODO метод должен быть назван также как в Rectangle
     def triangle_area(self) -> int or float:
         result = 0.5 * self.base * self.height
         return result
 
 
 class SquareMixin:
+    # TODO  метод для нахождения площади поверхности ничего не принимает
+    # TODO в методе объявляем переменную для подсчета
+    # TODO далее заводим цикл по переменной в которой как мы ожидаем будет список self.surfaces:
+    # TODO у объекта из списка мы вызываем метод area полученный результат записываем в переменную
+    # TODO возвращаем результат
+
     def square(self, size_x: int or float) -> int or float:
         self.size_x = size_x
         return self.size_x ** 2
@@ -51,8 +58,10 @@ class SquareMixin:
 class Cube(Square, SquareMixin):
     def __init__(self, length: int or float):
         super().__init__(length)
+        # TODO тут нужно определить этот список self.surfaces
 
 
 class Pyramid(Square, Triangle, SquareMixin):
     def __init__(self, length: int or float):
         super().__init__(length)
+        # TODO аналогично
