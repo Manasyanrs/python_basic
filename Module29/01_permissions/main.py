8e0cc92cf18cdde9c3a744973281f7afc46c29bc
Радик Манасян <arnara@inbox.ru>
2021-10-13 15:54:21 +0400

    Права доступа

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,1 +1,35 @@
-# TODO здесь писать код
+from typing import Callable
+
+
+def check_permission(arg: str) -> Callable:
+    def decorator(function: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            result = function(*args, **kwargs)
+            return result
+        try:
+            if arg in user_permissions:
+                return wrapper
+            raise PermissionError
+
+        except PermissionError:
+            print("PermissionError: У пользователя недостаточно прав, чтобы выполнить функцию {}".format(
+                function.__name__))
+
+    return decorator
+
+
+user_permissions = ['admin']
+
+
+@check_permission('admin')
+def delete_site():
+    print('Удаляем сайт')
+
+
+@check_permission('user_1')
+def add_comment():
+    print('Добавляем комментарий')
+
+
+delete_site()
+add_comment()
