3e2d5f81366f27a1ce1c1d2d52c500b667b9bb73
Радик Манасян <arnara@inbox.ru>
2021-10-13 15:56:38 +0400

    Синглтон

--- Module29/05_singleton/main.py
+++ Module29/05_singleton/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+import functools
+
+
+def singleton(cls):
+    @functools.wraps(cls)
+    def wrapper(*args, **kwargs):
+        if wrapper.wrapper_obj == 0:
+            wrapper.wrapper_obj = cls(*args, **kwargs)
+        return wrapper.wrapper_obj
+
+    wrapper.wrapper_obj = 0
+    return wrapper
+
+
+@singleton
+class Example:
+    pass
+
+
+my_obj = Example()
+my_another_obj = Example()
+
+print(id(my_obj))
+print(id(my_another_obj))
+
+print(my_obj is my_another_obj)
