6387f8561fb720f2d2b45333634e9999d62699c1
Радик Манасян <arnara@inbox.ru>
2021-10-13 15:56:14 +0400

    декоратор для декораторов

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+from typing import Callable
+import functools
+
+
+def decorator_with_args_for_any_decorator(decorator_function: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        result = decorator_function(*args, **kwargs)
+        return result
+    return wrapper
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(function: Callable, *args, **kwargs):
+    @functools.wraps(function)
+    def wrapper(*arg, **kwarg):
+        print("Переданные арги и кварги в декоратор:", args, kwargs)
+        result = function(*arg, **kwarg)
+        return result
+    return wrapper
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function(text="Юзер", num=101)
