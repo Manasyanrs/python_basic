4f91abb8c99a3005de81420f38ca89800e6babdd
Радик Манасян <arnara@inbox.ru>
2021-10-13 15:55:00 +0400

    Функция обратного вызова

--- Module29/02_callback/main.py
+++ Module29/02_callback/main.py
@@ -1,1 +1,26 @@
-# TODO здесь писать код
+from typing import Callable
+
+
+def callback(arg: str) -> Callable:
+    def decorator(function: Callable) -> Callable:
+        def wrapper(*args, **kwargs):
+            result = function(*args, **kwargs)
+            return result
+        return wrapper
+    return decorator
+
+
+@callback('//')
+def example():
+    print('Пример функции, которая возвращает ответ сервера')
+    return 'OK'
+
+
+app = {"//": example}
+
+route = app.get('//')
+if route:
+    response = route()
+    print('Ответ:', response)
+else:
+    print('Такого пути нет')
