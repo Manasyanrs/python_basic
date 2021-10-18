72951982cf9ccb5a81c0a13c0ea1a117753c9a1f
Радик Манасян <arnara@inbox.ru>
2021-10-18 17:32:14 +0400

    доработанное дз

--- Module29/01_permissions/main.py
+++ Module29/01_permissions/main.py
@@ -1,8 +1,17 @@
 from typing import Callable
+import functools
 
 
 def check_permission(arg: str) -> Callable:
+    """Декаратор функци на вход получает арумент и проверяет если такой аргумент есть в списке то декорирует функцию,
+    если нету то выводит на экран сообщение
+    :argument
+         arg: str (Передается названые аргумента)
+    :raise
+        У пользователя недостаточно прав, чтобы выполнить функцию + названые функции
+    """
     def decorator(function: Callable) -> Callable:
+        @functools.wraps(function)
         def wrapper(*args, **kwargs):
             result = function(*args, **kwargs)
             return result
@@ -23,16 +32,15 @@ user_permissions = ['admin']
 
 @check_permission('admin')
 def delete_site():
+    """Функция выводит на экран сообщение Удаляем сайт"""
     print('Удаляем сайт')
 
 
 @check_permission('user_1')
 def add_comment():
+    """Функция выводит на экран сообщение Добавляем комментарий"""
     print('Добавляем комментарий')
 
 
 delete_site()
 add_comment()
-
-# TODO во всех декораторах используем wraps
-# TODO пишем докстринги к каждому методу или функции
--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -3,6 +3,7 @@ import functools
 
 
 def decorator_with_args_for_any_decorator(decorator_function: Callable) -> Callable:
+    """ Декоратор на вход принемает другой декоратор """
     def wrapper(*args, **kwargs):
         result = decorator_function(*args, **kwargs)
         return result
@@ -10,21 +11,26 @@ def decorator_with_args_for_any_decorator(decorator_function: Callable) -> Calla
 
 
 @decorator_with_args_for_any_decorator
-def decorated_decorator(function: Callable, *args, **kwargs):
+def decorated_decorator(function: Callable, *dec_args, **dec_kwargs):
+    """ Декоратор на вход принемает функцию, не именованные аргументы и именованные аргументы
+     и выводит на экран сообщение (Переданные арги и кварги в декоратор: *args, **kwargs) """
     @functools.wraps(function)
-    def wrapper(*arg, **kwarg):
-        print("Переданные арги и кварги в декоратор:", args, kwargs)
-        result = function(*arg, **kwarg)
+    def wrapper(*func_args, **func_kwargs):
+        print("Переданные арги и кварги в декоратор:", dec_args, dec_kwargs)
+        result = function(*func_args, **func_kwargs)
         return result
     return wrapper
 
 
 @decorated_decorator(100, 'рублей', 200, 'друзей')
-# TODO нейминг параметров
-def decorated_function(text: str, num: int) -> None:
-    print("Привет", text, num)
+# TODO не пойму как передать нейминг параметров если декоратор принемает *args и **kwargs?
+def decorated_function(text: str, number: int) -> None:
+    """ Функция на вход принемает 2 аргумента и выводит на экран сообщение 'Привет первый аргумент и второй аргумент.
+    :argument
+        text: str
+        number: int """
+
+    print("Привет", text, number)
 
 
-decorated_function(text="Юзер", num=101)
-
-# TODO применить рекомендации данные ранее
+decorated_function(text="Юзер", number=101)
