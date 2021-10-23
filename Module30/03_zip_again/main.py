707ae75f0c2dd160c474dbbef5359145c47e6ce9
Радик Манасян <arnara@inbox.ru>
2021-10-23 21:21:24 +0400

    объединьяем два списка с помощью lambda функции

--- Module30/03_zip_again/main.py
+++ Module30/03_zip_again/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+from typing import List
+
+strings: List[str] = ['a', 'b', 'c', 'd', 'e']
+numbers: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
+
+results: List[tuple] = list(map(lambda letter, digit: (letter, digit), strings, numbers))
+
+print(results)
