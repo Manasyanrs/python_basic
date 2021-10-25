b79e248b7ec526257d841a7d13306cc4ac3fc629
Радик Манасян <arnara@inbox.ru>
2021-10-25 13:56:04 +0400

    доработанное дз

--- Module30/02_timeit/main.py
+++ Module30/02_timeit/main.py
@@ -1,31 +1,8 @@
 import timeit
-from typing import List
-
-# version1
-# TODO list в именовании не используем
-version_list_comprehensions: float = timeit.timeit('["".join(str(i) for i in range(100))]', number=10000)
-print("Время работы version_list_comprehensions = {} секунд(ы).".format(version_list_comprehensions))
-
-version_func_map: float = timeit.timeit('"".join(map(str, range(100)))', number=10000)
-print("Время работы version_func_map = {} секунд(ы).".format(version_func_map))
 
 
-# version2
-# TODO для чего эта функция ?
-def result_timer(argument: object) -> None:
-    """ Функция на вход принимает аргумент и выводит на экран время работы
-    :type argument: object
-    """
-    start = timeit.timeit()
-    # TODO не дописали что то
-    argument
-    end = timeit.timeit() - start
-    print("Время работы = {} секунд(ы).".format(end))
+version_1: float = timeit.timeit('["".join(str(i) for i in range(100))]', number=10000)
+print("Время работы version_list_comprehensions = {} секунд(ы).".format(version_1))
 
-
-print()
-version_comprehensions: List[str] = ["".join(str(digit) for digit in range(100))]
-result_timer(version_comprehensions)
-
-version_map: str = "".join(map(str, range(100)))
-result_timer(version_map)
+version_2: float = timeit.timeit('"".join(map(str, range(100)))', number=10000)
+print("Время работы version_func_map = {} секунд(ы).".format(version_2))
--- Module30/04_code_readability/main.py
+++ Module30/04_code_readability/main.py
@@ -1,12 +1,21 @@
-# TODO тут нужно использовать reduce
-list(filter(None, map(lambda y:y*reduce(lambda x, y: x*y != 0, map(lambda x, y=y: y % x, range(2, int(pow(y, 0.5) + 1))), 1), range(2, 1000))))
+import math
+from collections import Counter
 
-prime_numbers = list(filter(lambda x: all(map(lambda i: x % i != 0, range(2, x + 1))), range(1000)))
+prime_numbers = list(filter(lambda x: all(map(lambda i: x % i != 0, range(2, int(math.sqrt(x)) + 1))), range(2, 1000)))
 print(prime_numbers)
 
+prime_numbers_1 = [unique_element for unique_element, repeatable_element in Counter([digit for digit in range(1000)
+                                                                                     if digit != 0 and digit != 1
+                                                                                     for element in range(2, digit + 1)
+                                                                                     if digit % element == 0]).items()
+                   if repeatable_element == 1]
+print(prime_numbers_1)
 
-# TODO модуль функционального программирования подразумевает запись в строку
-prime_numbers_1: list = []
+# TODO по дз потребовался
+# TODO 1. “однострочным” кодом, без объявления дополнительных функций;
+# TODO 2. обычным, “своим” кодом, который покажется вам наиболее красивым.
+# TODO такой вариант по мне еще лучше и понятнее
+prime_numbers_2: list = []
 temporary_variable: int = 0
 
 for digit in range(1000):
@@ -15,10 +24,9 @@ for digit in range(1000):
             if digit % element == 0:
                 temporary_variable += 1
         if temporary_variable <= 1:
-            prime_numbers_1.append(digit)
+            prime_numbers_2.append(digit)
             temporary_variable = 0
         else:
             temporary_variable = 0
 
-
-print(prime_numbers_1)
+print(prime_numbers_2)
