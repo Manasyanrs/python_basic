78487cb9125ff875d03f16f545dcb62c4f5e0a22
user74 <housechel@mail.ru>
2021-10-29 14:32:41 +0500

    Lesson 031 05,06 - Corrected

--- Module31/05_pin_code/main.py
+++ Module31/05_pin_code/main.py
@@ -1,5 +1,7 @@
 import itertools
 
 
+# TODO используем атрибут repeat=4
+# TODO range(10) выносим в переменную
 for code in itertools.product(range(10), range(10), range(10), range(10)):
     print(code)
--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,8 +1,11 @@
 import requests
 import re
 
-# TODO Выводится лишный сивол(</h3>) в резултате
 url = requests.get("http://www.columbia.edu/~fdc/sample.html")
 text = url.text
+
+# TODO во первых укажите от до h3
+# TODO используйте группировку символов ()
+# TODO +</h3> автоматом плюсует его к выводу
 result = re.findall(r"[^>]+</h3>", text)
 print(result)
